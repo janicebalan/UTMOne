bc3d8a73a52ce9819d4569921c44ece1cdc9778f
Judith Aloysius <judith301199@gmail.com>
2020-11-22 16:45:42 +0800

    deleted user and superadmin roles from db

--- LIGHTS/roles.sql
+++ LIGHTS/roles.sql
@@ -1,5 +1,3 @@
-INSERT INTO `roles` VALUES (1, 'superadministrator', 'Superadministrator', 'Superadministrator', '2020-11-21 14:00:15', '2020-11-21 14:00:15');
-INSERT INTO `roles` VALUES (2, 'administrator', 'Administrator', 'Administrator', '2020-11-21 14:00:19', '2020-11-21 14:00:19');
-INSERT INTO `roles` VALUES (3, 'user', 'User', 'User', '2020-11-21 14:00:20', '2020-11-21 14:00:20');
-INSERT INTO `roles` VALUES (4, 'lecturer', 'Lecturer', 'Lecturer', '2020-11-21 14:00:22', '2020-11-21 14:00:22');
-INSERT INTO `roles` VALUES (5, 'student', 'Student', 'Student', '2020-11-21 14:00:24', '2020-11-21 14:00:24');
+INSERT INTO `roles` VALUES (1, 'administrator', 'Administrator', 'Administrator', '2020-11-21 14:00:19', '2020-11-21 14:00:19');
+INSERT INTO `roles` VALUES (2, 'lecturer', 'Lecturer', 'Lecturer', '2020-11-21 14:00:22', '2020-11-21 14:00:22');
+INSERT INTO `roles` VALUES (3, 'student', 'Student', 'Student', '2020-11-21 14:00:24', '2020-11-21 14:00:24');
--- LIGHTS/users.sql
+++ LIGHTS/users.sql
@@ -1,6 +1,4 @@
-INSERT INTO `users` VALUES (1, 'Superadministrator', 'superadministrator@app.com', NULL, '$2y$10$ddvA8m4Raigs0rPuEWFV2OFRjm0LxVaVHZN8.7ysxN7Hh.ta7zNLm', 'mH5QXwdUXDjQEUzmGMm4SCcYG8CNeaWawGPLU3oJhC0RTDqrRxzjGqyEjUBM', '2020-11-21 14:00:19', '2020-11-21 14:00:19');
-INSERT INTO `users` VALUES (2, 'Administrator', 'administrator@app.com', NULL, '$2y$10$r/jEW1kGqysylbHtN5aWAu4ln8ixqsZGB4HalGnFLjidyUi9RE2ly', NULL, '2020-11-21 14:00:20', '2020-11-21 14:00:20');
-INSERT INTO `users` VALUES (3, 'User', 'user@app.com', NULL, '$2y$10$b9RLyyP0Wh.Tp0wVPonQle88YaISzCF8oh6yGGjsfcs5KZqBTzUMW', NULL, '2020-11-21 14:00:21', '2020-11-21 14:00:21');
-INSERT INTO `users` VALUES (4, 'Lecturer', 'lecturer@app.com', NULL, '$2y$10$PS1hxWCa.w7hpomWGIAfBOC6JOnJdExytr7u31BAZmlgUgfngzhQC', NULL, '2020-11-21 14:00:23', '2020-11-21 14:00:23');
-INSERT INTO `users` VALUES (5, 'Student', 'student@app.com', NULL, '$2y$10$9H3zjVXFB0YWYvavmO8hM.j7Gd15zgMiGQPMvtr5y2QSaoxoZoXTm', NULL, '2020-11-21 14:00:25', '2020-11-21 14:00:25');
-INSERT INTO `users` VALUES (6, 'Cru User', 'cru_user@app.com', NULL, '$2y$10$5qTFxoEwJwmcY7giKdhnauZBTX7eiLDrw1WRjawrAJJrrCf8koIRW', 'hKt0LYjroS', '2020-11-21 14:00:25', '2020-11-21 14:00:25');
+INSERT INTO `users` VALUES (1, 'Administrator', 'administrator@app.com', NULL, '$2y$10$r/jEW1kGqysylbHtN5aWAu4ln8ixqsZGB4HalGnFLjidyUi9RE2ly', NULL, '2020-11-21 14:00:20', '2020-11-21 14:00:20');
+INSERT INTO `users` VALUES (2, 'Lecturer', 'lecturer@app.com', NULL, '$2y$10$PS1hxWCa.w7hpomWGIAfBOC6JOnJdExytr7u31BAZmlgUgfngzhQC', NULL, '2020-11-21 14:00:23', '2020-11-21 14:00:23');
+INSERT INTO `users` VALUES (3, 'Student', 'student@app.com', NULL, '$2y$10$9H3zjVXFB0YWYvavmO8hM.j7Gd15zgMiGQPMvtr5y2QSaoxoZoXTm', NULL, '2020-11-21 14:00:25', '2020-11-21 14:00:25');
+INSERT INTO `users` VALUES (4, 'Cru User', 'cru_user@app.com', NULL, '$2y$10$5qTFxoEwJwmcY7giKdhnauZBTX7eiLDrw1WRjawrAJJrrCf8koIRW', 'hKt0LYjroS', '2020-11-21 14:00:25', '2020-11-21 14:00:25');
