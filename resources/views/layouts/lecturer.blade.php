244cab36a8e9da2a5804aa2e5f127686bb6b16d2
Judith Aloysius <judith301199@gmail.com>
2020-12-26 11:09:37 +0800

    refine

--- resources/views/layouts/lecturer.blade.php
+++ resources/views/layouts/lecturer.blade.php
@@ -39,7 +39,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
+        <a href="lecturer" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
       <a class="nav-link" href="{{ route('logout') }}"
