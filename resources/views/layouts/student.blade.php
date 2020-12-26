ca0c8b640048480a9db73a4c993b2356bf407a68
Judith Aloysius <judith301199@gmail.com>
2020-12-26 10:42:04 +0800

    add logout button to student page

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -42,7 +42,15 @@
         <a href="index3.html" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+      <a class="nav-link" href="{{ route('logout') }}"
+          onclick="event.preventDefault();
+          document.getElementById('logout-form').submit();">
+          {{ __('Logout') }}
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+          @csrf
+        </form>
       </li>
     </ul>
 
