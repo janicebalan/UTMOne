77fc22043408f4dfd3e43b513169fe2b3b0c7335
poorbugger <69106288+poorbugger@users.noreply.github.com>
2021-02-13 13:12:27 +0800

    update db connection

--- database/migrations/2021_02_12_143646_add_user_id_to_courses.php
+++ database/migrations/2021_02_12_143646_add_user_id_to_courses.php
@@ -15,9 +15,11 @@ class AddUserIdToCourses extends Migration
     {
         Schema::table('courses', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id')->default('0');
+			//$table->dropPrimary();
+			//$table->primary(['id', 'user_id']);
         });
 		
-		DB::unprepared('ALTER TABLE "courses" DROP PRIMARY KEY, ADD PRIMARY KEY (  "id" ,  "user_id" )');
+		DB::unprepared('ALTER TABLE `courses` DROP PRIMARY KEY, ADD PRIMARY KEY (  `id` ,  `user_id` )');
 		
 		Schema::table('courses', function (Blueprint $table) {
 		$table->foreign('user_id')->references('id')->on('users');
