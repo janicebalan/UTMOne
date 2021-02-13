b9bb3e257d7cd796bf91ee7427b79e1e996e38ca
Janice Balan <janice.balan18@gmail.com>
2021-02-13 08:18:21 +0800

    edited mainpage of student ui

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -120,7 +120,6 @@ $(document).ready(function () {
               <i class="nav-icon fas fa-copy"></i>
               <p>
                 Enroll Courses
-                <i class="fas fa-angle-left right"></i>
               </p>
             </a>
             </li>
@@ -154,45 +153,7 @@ $(document).ready(function () {
               </li>
             </ul>
           </li>
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon far fa-envelope"></i>
-              <p>
-                Chatroom
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="pages/mailbox/mailbox.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inbox</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/mailbox/compose.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Compose</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/mailbox/read-mail.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Read</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-
-          <li class="nav-header">COURSE ACTION</li>
-          <li class="nav-item">
-            <a href="{{route('studTaskList')}}" class="nav-link ">
-              <i class="nav-icon fa fa-th"></i>
-              <p>
-                Task Dashboard
-              </p>
-            </a>
-            </li>
+          
 
 
 
--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -24,63 +24,19 @@
     <div class="container-fluid">
       <!-- Small boxes (Stat box) -->
       <div class="row">
-        <div class="col-lg-3 col-6">
+        <div class="col-sm-4">
           <!-- small box -->
-          <div class="small-box bg-info">
-            <div class="inner">
-              <h3>150</h3>
-
-              <p>New Orders</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-bag"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <div class="small-box ">
+          <img src="{{asset ('dist/img/utmlogo.png')}}" alt="UTM Logo" width="300px" height="100px">
           </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-6">
+        <div class="col-sm-8 col-6" >
           <!-- small box -->
-          <div class="small-box bg-success">
-            <div class="inner">
-              <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-              <p>Bounce Rate</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <div class="small-box" style="background-color:#1a6765">
+          <div class="container" style="padding: 5px 50px 5px 20px" >
+            <h1 style="font-size:70px; color:white"> Welcome to UTMOne </h1>
           </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-warning">
-            <div class="inner">
-              <h3>44</h3>
-
-              <p>User Registrations</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-danger">
-            <div class="inner">
-              <h3>65</h3>
-
-              <p>Unique Visitors</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-pie-graph"></i>
-            </div>
-            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
@@ -111,16 +67,6 @@
                 <i class="fas fa-chart-pie mr-1"></i>
                 Courses
               </h3>
-              <div class="card-tools">
-                <ul class="nav nav-pills ml-auto">
-                  <li class="nav-item">
-                    <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
-                  </li>
-                </ul>
-              </div>
             </div><!-- /.card-header -->
             <div class="card-body">
               <div class="tab-content p-0">
@@ -158,52 +104,23 @@
         </section>
         <!-- /.Left col -->
         <!-- right col (We are only adding the ID to make the widgets sortable)-->
-        <section class="col-lg-5 connectedSortable">
+        <div class="col-lg-5">
 
           <!-- Map card -->
-          <div class="card bg-gradient-primary">
-            <div class="card-header border-0">
-              <h3 class="card-title">
-                <i class="fas fa-map-marker-alt mr-1"></i>
-                Visitors
-              </h3>
-              <!-- card tools -->
-              <div class="card-tools">
-                <button type="button"
-                        class="btn btn-primary btn-sm daterange"
-                        data-toggle="tooltip"
-                        title="Date range">
-                  <i class="far fa-calendar-alt"></i>
-                </button>
-                <button type="button"
-                        class="btn btn-primary btn-sm"
-                        data-card-widget="collapse"
-                        data-toggle="tooltip"
-                        title="Collapse">
-                  <i class="fas fa-minus"></i>
-                </button>
-              </div>
-              <!-- /.card-tools -->
-            </div>
-            <div class="card-body">
-              <div id="world-map" style="height: 250px; width: 100%;"></div>
-            </div>
+          <div class="card bg-gradient-primary" style="display: none;">
             <!-- /.card-body-->
-            <div class="card-footer bg-transparent">
+            <div class="card-footer bg-transparent" style="display: none;">
               <div class="row">
                 <div class="col-4 text-center">
-                  <div id="sparkline-1"></div>
-                  <div class="text-white">Visitors</div>
+                  <div id="sparkline-1" style="display: none;"></div>
                 </div>
                 <!-- ./col -->
                 <div class="col-4 text-center">
-                  <div id="sparkline-2"></div>
-                  <div class="text-white">Online</div>
+                  <div id="sparkline-2" style="display: none;"></div>
                 </div>
                 <!-- ./col -->
                 <div class="col-4 text-center">
-                  <div id="sparkline-3"></div>
-                  <div class="text-white">Sales</div>
+                  <div id="sparkline-3" style="display: none;"></div>
                 </div>
                 <!-- ./col -->
               </div>
@@ -245,14 +162,14 @@
               <!-- /. tools -->
             </div>
             <!-- /.card-header -->
-            <div class="card-body pt-0">
+            <div class="card-body">
               <!--The calendar -->
               <div id="calendar" style="width: 100%"></div>
             </div>
             <!-- /.card-body -->
           </div>
           <!-- /.card -->
-        </section>
+        </div>
         <!-- right col -->
       </div>
       <!-- /.row (main row) -->
